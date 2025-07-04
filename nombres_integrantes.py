e226cd6d219eeeab7ee560a4b9b47a2c5c33e1ec
TheMafia898 <igna.faundez@duocuc.cl>
2025-07-04 03:22:59 +0000

    Agrega scripts iniciales de listado de nombres y clasificación de VLAN

---
+++ nombres_integrantes.py
@@ -0,0 +1,11 @@
+
+integrantes = [
+    "Ignacio Faundez",
+    "Benjamin Martinez",
+    "Franco Rojas",
+    "Daniel Gallardo"
+]
+
+print("Ignacio faundez Benjamin Martinez Franco Rojas Daniel Gallardo :")
+for nombre in integrantes:
+    print("-", nombre)
